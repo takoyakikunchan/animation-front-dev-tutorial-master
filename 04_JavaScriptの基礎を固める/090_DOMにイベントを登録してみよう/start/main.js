e9ddf57b8768b5609ac048a72db55a8653eb2e60
takoyakikunchan <66656447+takoyakikunchan@users.noreply.github.com>
2020-11-06 10:24:17 +0900

    jsで文字列を分割。（ループをreduceで）

--- 04_JavaScriptの基礎を固める/090_DOMにイベントを登録してみよう/start/main.js
+++ 04_JavaScriptの基礎を固める/090_DOMにイベントを登録してみよう/start/main.js
@@ -2,7 +2,7 @@ const btn = document.querySelector('#btn');
 const h1 = document.querySelector('h1');
 function hello() {
     alert('hello');
-    // this.style.color = 'red';
+    // this.style.color = 'red';  要素を関数内で取得する場合はthisに置き換えられる
 }
 function changeColor() {
     h1.style.color = 'red';
@@ -10,6 +10,11 @@ function changeColor() {
 function changeBgColor() {
     h1.style.backgroundColor = 'green';
 }
+
+
+
 btn.addEventListener('click', changeColor);
 btn.addEventListener('click', changeBgColor);
 btn.removeEventListener('click', changeBgColor);
+
+btn.addEventListener('mouseenter', changeColor); //‘mouseenter'は要素の中にマウスがhoverし入った時
